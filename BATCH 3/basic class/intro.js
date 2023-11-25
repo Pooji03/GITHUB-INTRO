c4936185b752a19f4c7b177274e54df6cbb8cd74
Pooji03 <151988806+Pooji03@users.noreply.github.com>
2023-11-24 22:15:39 -0800

    Add files via upload

---
+++ BATCH 3/basic class/intro.js
@@ -0,0 +1,12 @@
+
+document.write("hello world");
+console.log("hello world");
+alert("welcome to java script");
+var a=10;
+var b=20;
+document.write(a+b);
+{
+    let d=40
+
+}
+document.write(d);
